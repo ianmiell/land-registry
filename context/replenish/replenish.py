edb6e7be7f302c29969094140ea8cfa960412496
Ian Miell <ian.miell@gmail.com>
2016-01-10 21:26:15 +0000

    insert

--- context/replenish/replenish.py
+++ context/replenish/replenish.py
@@ -11,7 +11,8 @@ class replenish(ShutItModule):
 		shutit.send(r'''wget -qO- http://publicdata.landregistry.gov.uk/market-trend-data/price-paid-data/a/pp-complete.csv | sed 's/\r//g' > /tmp/all.csv''',timeout=99999)
 		if shutit.cfg[self.module_id]['seed'] == 'Y':
 			shutit.send(r'''cat <(cat <(echo ID,PRICE,DATE,POSTCODE,TYPE,NEW_BUILD,ESTATE_TYPE,BUILDING_NO,BUILDING_NAME,STREET,TOWN,CITY,DISTRICT,COUNTY,EXTRA1,EXTRA2) <(cat /tmp/all.csv)) > /tmp/insert.csv''')
-			shutit.send('''echo "copy land_registry (id, price, transaction_date, postcode, type, new_build, estate_type, building_1, building_2, street, town, city, district, county, extra1, extra2) from '/tmp/insert.csv' delimiter ',' csv header;" | psql -U postgres -h landregistrydb land_registry''')
+			shutit.send('''echo "copy land_registry (id, price, transaction_date, postcode, type, new_build, estate_type, building_1, building_2, street, town, city, district, county, extra1, extra2) from STDIN delimiter ',' csv header;" >> /tmp/insert.csv''')
+			shutit.send('''psql -U postgres -h landregistrydb land_registry < /tmp/insert.csv''')
 		else:
 			shutit.send('''echo "copy (select id, price, transaction_date, postcode, type, new_build, estate_type, building_1, building_2, street, town, city, district, county, extra1, extra2 from land_registry) to STDOUT with (format csv, delimiter ',', force_quote *)" | psql -U postgres -h landregistrydb land_registry > /tmp/lr.csv''')
 			shutit.send('''sort /tmp/all.csv | sed 's/ 00:00//' > /tmp/all.csv.sorted''')
